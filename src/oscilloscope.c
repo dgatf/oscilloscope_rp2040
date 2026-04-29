d0330da1b1532f57974c68f07424a70008843157
Daniel <danielgorbea@gmail.com>
2026-04-29 06:26:50 -0400

    Use USB library as submodule

--- src/oscilloscope.c
+++ src/oscilloscope.c
@@ -1,6 +1,6 @@
 /*
  * Oscilloscope RP2040
- * Copyright (C) 2024 Daniel Gorbea <danielgorbea@hotmail.com>
+ * Copyright (C) 2024-2026 Daniel Gorbea <danielgorbea@hotmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
